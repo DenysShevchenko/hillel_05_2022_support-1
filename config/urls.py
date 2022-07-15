d9528e98089cbdbf920804cb4f926dabe01ea574
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-07-15 10:42:33 +0300

    Upload exchange rates example

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from config.api import history, home, btc_usd
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("home/", home),
+    path("btc_usd/", btc_usd),
+    path("history/", history)
 ]
